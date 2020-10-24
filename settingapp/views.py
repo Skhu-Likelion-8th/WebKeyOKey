6b674426541f83059bcbb69c20444ecb1cbbec82
HamJinkyoung <wlsrud3803@gmail.com>
2020-10-25 02:38:31 +0900

    링크연결, 이미지 수정

--- settingapp/views.py
+++ settingapp/views.py
@@ -49,3 +49,45 @@ def bye(request):
 
 def option(request):
     return render(request, 'settingapp/option.html')
+
+# 달력구현
+import datetime
+import calendar
+from .calendar import Calendar
+from django.utils.safestring import mark_safe
+
+def sales(request):
+    today = get_date(request.GET.get('month'))
+
+    prev_month_var = prev_month(today)
+    next_month_var = next_month(today)
+
+    cal = Calendar(today.year, today.month)
+    html_cal = cal.formatmonth(withyear=True)
+    result_cal = mark_safe(html_cal)
+
+    context = {'calendar' : result_cal, 'prev_month' : prev_month_var, 'next_month' : next_month_var}
+
+    return render(request, 'settingapp/sales.html', context)
+
+#현재 달력을 보고 있는 시점의 시간을 반환
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return datetime.date(year, month, day=1)
+    return datetime.datetime.today()
+
+#현재 달력의 이전 달 URL 반환
+def prev_month(day):
+    first = day.replace(day=1)
+    prev_month = first - datetime.timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+#현재 달력의 다음 달 URL 반환
+def next_month(day):
+    days_in_month = calendar.monthrange(day.year, day.month)[1]
+    last = day.replace(day=days_in_month)
+    next_month = last + datetime.timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
